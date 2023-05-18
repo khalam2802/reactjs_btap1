41644df31fb0b26372cbb4a9fd6887e52c3927da
Your name <your@email.com>
2023-05-18 18:25:42 +0700

    update2

---
+++ src/BaiTapThucHanhLayout/Item.js
@@ -0,0 +1,130 @@
+import React, { Component } from "react";
+
+export default class Item extends Component {
+  render() {
+    return (
+      <div>
+        <div className="container">
+          <div className="row">
+            
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                    <i class="fab fa-facebook"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>Fresh new layout</h1>
+                  <p>
+                    With Bootstrap 5, we've created a fresh new layout for this
+                    template!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                    <i class="fa fa-download"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>Free to download</h1>
+                  <p>
+                  As always, Start Bootstrap has a powerful collectin of free templates.
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                    <i class="fa fa-file-alt"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>Jumbotron hero header</h1>
+                  <p>
+                  The heroic part of this template is the jumbotron hero header!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                <i class="fab fa-bimobject"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>Feature boxes</h1>
+                  <p>
+                  We've created some custom feature boxes using Bootstrap icons!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                   <i class="fa fa-code"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>Simple clean code</h1>
+                  <p>
+                  We keep our dependencies up to date and squash bugs as they come!
+                  </p>
+                </div>
+              </div>
+            </div>
+            <div className="col-4 mb-4">
+              <div className="content text-center bg-light">
+                <div className="content_body ">
+                  <a
+                    className="item bg-primary mt-n4"
+                    href="
+                  "
+                  >
+                 <i class="fa fa-check-circle"></i>
+                  </a>
+                </div>
+                <div className="content_footer">
+                  <h1>A name you trust</h1>
+                  <p>
+                  Start Bootstrap has been the leader in free Bootstrap templates since 2013!
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
